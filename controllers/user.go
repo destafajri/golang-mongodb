10fd9413acea8816dd69f7e71d5c75d6159d5678
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-07 17:43:55 +0700

    update

--- controllers/user.go
+++ controllers/user.go
@@ -18,3 +18,14 @@ func NewUserController(s *mgo.Session) *UserController{
 	return &UserController{s}
 }
 
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params){
+	
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params){
+
+}
