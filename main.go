49fb86aaa8ed0e0cc34655f77bed0e63a7ade229
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-07 17:31:26 +0700

    update

--- main.go
+++ main.go
@@ -4,8 +4,26 @@ import(
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 	"net/http"
+
+	"github.com/destafajri/golang-mongodb/controllers"
 )
 
 func main() {
 	
+	r := httprouter.New()
+	usercontrol := controllers.NewUserController(getSession)
+	r.GET("/user/:id", usercontrol.GetUser)
+	r.POST("/user", usercontrol.CreateUser)
+	r.DELETE("/user/:id", usercontrol.DeleteUser)
+
+}
+
+func getSession() *mgo.Session{
+
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil{
+		panic(err)
+	}
+	return s
+
 }
